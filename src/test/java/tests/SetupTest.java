32fd5afd8337d1d5b6213ff7e82b9e59167b8f85
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-10 19:27:10 -0300

    Incluidos novos testes

--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utils.Browser;
 import utils.Utils;
 
@@ -12,5 +13,22 @@ public class SetupTest extends BaseTests{
     public void testOpeningBrowserAndLoadingPage(){
         assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseURL()));
         System.out.println("Abrimos o navegador e carregamos a url!");
+    }
+
+    @Test
+    public void testLogin(){
+        Browser.getCurrentDriver().findElement(By.className("login")).click();
+        System.out.println("Click login");
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getLoginPageURL()));
+        Browser.getCurrentDriver().findElement(By.id("email")).sendKeys("gustavoullmann@gmail.com");
+        System.out.println("Inseriu e-mail");
+        Browser.getCurrentDriver().findElement(By.id("passwd")).sendKeys("12345");
+        System.out.println("Inseriu senha");
+        Browser.getCurrentDriver().findElement(By.id("SubmitLogin")).click();
+        System.out.println("Click sign in");
+        assertTrue((Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getLoggedUserPageURL())));
+        System.out.println("Logado com sucesso");
+        assertTrue(Browser.getCurrentDriver().findElement(By.className("page-heading")).getText().contains("MY ACCOUNT"));
+        System.out.println("Pagina de usuário carregada com sucesso");
     }
 }
