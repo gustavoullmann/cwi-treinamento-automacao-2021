5a651d0efc3ecc9a323cf31a1c3cffa07c0cc8ca
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-10 20:56:32 -0300

    adicionados get URL para loginPage e loggedPage

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -5,4 +5,12 @@ public class Utils {
     public static String getBaseURL(){
         return "http://automationpractice.com/";
     }
+
+    public static String getLoginPageURL(){
+        return "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+    }
+
+    public static String getLoggedUserPageURL(){
+        return "http://automationpractice.com/index.php?controller=my-account";
+    }
 }
