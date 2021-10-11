96c5c8c0909a321d55ad57a4e4a516cbeb294083
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-11 17:47:46 -0300

    adicionado retorno de URL para resultado de busca por 'dress'

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -13,4 +13,8 @@ public class Utils {
     public static String getLoggedUserPageURL(){
         return "http://automationpractice.com/index.php?controller=my-account";
     }
+
+    public static String getSearchByDressPageURL(){
+        return "http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=dress&submit_search=";
+    }
 }
