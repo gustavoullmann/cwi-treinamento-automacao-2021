f137b05ce937058e2325ca91546f488f51700358
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-11 10:48:15 -0300

    criado construtor e clickBtnLogin

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,2 +1,17 @@
-package pageObjects;public class HomePage {
+package pageObjects;
+
+import elementMapper.HomePageElementMapper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+import utils.Browser;
+
+public class HomePage extends HomePageElementMapper {
+
+    public HomePage(){
+        PageFactory.initElements(Browser.getCurrentDriver(),this);
+    }
+
+    public void clickBtnLogin(){
+        login.click();
+    }
 }
