5e1a3b30676c45fb40bf193bdf9ffc1d93cc8249
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-11 10:49:49 -0300

    criado construtor, fillEmail, fillPasswd e clickSubmitBtn

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,2 +1,24 @@
-package pageObjects;public class LoginPage {
+package pageObjects;
+
+import elementMapper.LoginPageElementMapper;
+import org.openqa.selenium.support.PageFactory;
+import utils.Browser;
+
+public class LoginPage extends LoginPageElementMapper {
+
+    public LoginPage(){
+        PageFactory.initElements(Browser.getCurrentDriver(),this);
+    }
+
+    public void fillEmail(){
+        email.sendKeys("gustavoullmann@gmail.com");
+    }
+
+    public void fillPasswd(){
+       passwd.sendKeys("12345");
+    }
+
+    public void clickBtnSubmitLogin(){
+        SubmitLogin.click();
+    }
 }
