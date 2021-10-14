ae4c810ac92e4f1aa221634e06a7cd36649ac263
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-14 19:00:16 -0300

    criado mouse over

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -1,2 +1,13 @@
-package pageObjects;public class BasePage {
+package pageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utils.Browser;
+
+public class BasePage extends Browser {
+
+    public static void mouseOver(WebElement element) {
+        Actions action = new Actions(Browser.getCurrentDriver());
+        action.moveToElement(element).build().perform();
+    }
 }
