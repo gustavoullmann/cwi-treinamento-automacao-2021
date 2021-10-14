713c62a238d01c223901b0600559b1622978d347
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-13 23:20:54 -0300

    criado webElement nameCategoryTShirts

--- src/test/java/elementMapper/CategoryPageElementMapper.java
+++ src/test/java/elementMapper/CategoryPageElementMapper.java
@@ -1,2 +1,11 @@
-package elementMapper;public class CategoryPageElementMapper {
+package elementMapper;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CategoryPageElementMapper {
+
+    @FindBy(className = "cat-name")
+    public WebElement nameCategoryTShirts;
+
 }
