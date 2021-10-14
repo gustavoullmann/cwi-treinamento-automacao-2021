9db162daf3c5a4fbc014718c81d73685c2cf7899
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-13 23:26:04 -0300

    criado métodos: page factory, isPageTShirts, getAuthPageTShirt

--- src/test/java/pageObjects/CategoryPage.java
+++ src/test/java/pageObjects/CategoryPage.java
@@ -1,2 +1,23 @@
-package pageObjects;public class CategoryPage {
+package pageObjects;
+
+import elementMapper.CategoryPageElementMapper;
+import org.openqa.selenium.support.PageFactory;
+import utils.Browser;
+
+public class CategoryPage extends CategoryPageElementMapper {
+
+    public CategoryPage() {
+        PageFactory.initElements(Browser.getCurrentDriver(), this);
+    }
+
+    public boolean isPageTShirts() {
+        return getAuthenticationPageTShirts().contains("T-SHIRTS");
+    }
+
+    public String getAuthenticationPageTShirts() {
+        return nameCategoryTShirts.getText();
+    }
+
+
+
 }
