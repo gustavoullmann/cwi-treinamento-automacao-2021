3dee36d2ea239f2b859bc75a8500f05a57b216c7
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-15 16:34:43 -0300

    import de dependencia e inclusao de @Steps nos metodos

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,6 +1,7 @@
 package pageObjects;
 
 import elementMapper.LoginPageElementMapper;
+import io.qameta.allure.Step;
 import org.openqa.selenium.support.PageFactory;
 import utils.Browser;
 
@@ -10,14 +11,17 @@ public class LoginPage extends LoginPageElementMapper {
         PageFactory.initElements(Browser.getCurrentDriver(),this);
     }
 
+    @Step("Preencheu o email")
     public void fillEmail(){
         email.sendKeys("gustavoullmann@gmail.com");
     }
 
+    @Step("Preencheu a senha")
     public void fillPasswd(){
        passwd.sendKeys("12345");
     }
 
+    @Step("Clicou em Sign In")
     public void clickBtnSubmitLogin(){
         SubmitLogin.click();
     }
