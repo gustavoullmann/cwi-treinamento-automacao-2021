14f95559f9fe645058171696cacb8b20e97c540f
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-14 23:04:13 -0300

    criado construtor e getNameCart

--- src/test/java/pageObjects/CartPage.java
+++ src/test/java/pageObjects/CartPage.java
@@ -1,4 +1,17 @@
 package pageObjects;
 
-public class CartPage {
+import elementMapper.CartPageElementMapper;
+import org.openqa.selenium.support.PageFactory;
+import utils.Browser;
+
+public class CartPage extends CartPageElementMapper {
+
+    public CartPage() {
+        PageFactory.initElements(Browser.getCurrentDriver(), this);
+    }
+
+    public String getNameProductCart() {
+        return productNameCart.getText();
+    }
+
 }
