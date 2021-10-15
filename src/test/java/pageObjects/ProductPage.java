b8dfc9a81fcba40c68c5e9d53c986336e79eeae9
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-14 23:05:19 -0300

    criados clicks: addToCart e ProceedCheckout

--- src/test/java/pageObjects/ProductPage.java
+++ src/test/java/pageObjects/ProductPage.java
@@ -13,4 +13,12 @@ public class ProductPage extends ProductPageElementMapper {
     public String getProductNamePDP() {
         return productNamePDP.getText();
     }
+
+    public void ClickButtonAddToCart() {
+        buttonAddToCart.click();
+    }
+
+    public void clickButtonModalProceedToCheckout() {
+        buttonModalProceedToCheckout.click();
+    }
 }
