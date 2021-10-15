e6ce5bba7199a280c6e763497b0fe79a13e87f43
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-14 23:01:31 -0300

    criado findby para productNameCart

--- src/test/java/elementMapper/CartPageElementMapper.java
+++ src/test/java/elementMapper/CartPageElementMapper.java
@@ -1,2 +1,10 @@
-package elementMapper;public class CartPageElementMapper {
+package elementMapper;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CartPageElementMapper {
+
+    @FindBy(css = ".cart_description .product-name")
+    public WebElement productNameCart;
 }
