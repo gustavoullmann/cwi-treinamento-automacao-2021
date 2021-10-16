b8497a7345bfced1fb631a9a5f5f984b60a75f50
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 18:09:18 -0300

    criados os @findbys

--- src/test/java/elementMapper/OrderPageElementMapper.java
+++ src/test/java/elementMapper/OrderPageElementMapper.java
@@ -1,2 +1,25 @@
-package elementMapper;public class OrderPageElementMapper {
+package elementMapper;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class OrderPageElementMapper {
+
+    @FindBy(css = ".cart_navigation a[title='Proceed to checkout']")
+    public WebElement proceedToCheckoutButton;
+
+    @FindBy(css = ".cart_navigation [name='processAddress']")
+    public WebElement proceedToCheckoutButtonStep1;
+
+    @FindBy(id = "cgv")
+    public WebElement agreeToTheTermsCheckBox;
+
+    @FindBy(css = ".cart_navigation [name=processCarrier]")
+    public WebElement proceedToCheckoutButtonStep2;
+
+    @FindBy(css = ".payment_module [title='Pay by bank wire']")
+    public WebElement payByBankWirePaymentOption;
+
+    @FindBy(css = ".cart_navigation [type='submit']")
+    public WebElement confirmOrder;
 }
