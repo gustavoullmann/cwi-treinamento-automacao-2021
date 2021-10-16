294a42e767d755203573c2970d97204e2afdfbdf
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 18:08:32 -0300

    criados os @findbys

--- src/test/java/elementMapper/MyAccountElementMapper.java
+++ src/test/java/elementMapper/MyAccountElementMapper.java
@@ -1,5 +1,13 @@
 package elementMapper;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MyAccountElementMapper {
 
+    @FindBy(id = "search_query_top")
+    public WebElement searchQuery;
+
+    @FindBy(name = "submit_search")
+    public WebElement submitSearch;
 }
