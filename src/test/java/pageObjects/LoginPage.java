e058fbf71bcfe3e68e9ad6de54b522bf48aa178e
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 00:45:51 -0300

    renomeados métodos, criados fill email e click create account

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -5,24 +5,37 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.support.PageFactory;
 import utils.Browser;
 
+import java.util.Random;
+
 public class LoginPage extends LoginPageElementMapper {
 
-    public LoginPage(){
+    public LoginPage() {
         PageFactory.initElements(Browser.getCurrentDriver(),this);
     }
 
     @Step("Preencheu o email")
-    public void fillEmail(){
+    public void fillEmail() {
         email.sendKeys("gustavoullmann@gmail.com");
     }
 
     @Step("Preencheu a senha")
-    public void fillPasswd(){
-       passwd.sendKeys("12345");
+    public void fillPasswd() {
+        passwd.sendKeys("12345");
     }
 
     @Step("Clicou em Sign In")
-    public void clickBtnSubmitLogin(){
-        SubmitLogin.click();
+    public void clickBtnSubmitLogin() {
+        submitLogin.click();
+    }
+
+    @Step("Preencheu email de criação de cadastro")
+    public void fillAccountCreationEmail() {
+        int randomInt = new Random().nextInt(10000) + 1;
+        createAccountEmail.sendKeys("teste" + String.valueOf(randomInt) + "@mail.com");
+    }
+
+    @Step("Clicou em Create an account")
+    public void createAnAccountClickButton() {
+        createAnAccountButton.click();
     }
 }
