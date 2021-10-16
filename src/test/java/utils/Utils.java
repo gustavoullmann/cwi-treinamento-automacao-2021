597b5ed0ddd98617ae2d4ab9d2c9056c608d9d79
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 00:50:06 -0300

    métodos renomeados e getAccountCreationURL com novo return

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -2,19 +2,23 @@ package utils;
 
 public class Utils {
 
-    public static String getBaseURL(){
+    public static String getHomePageURL() {
         return "http://automationpractice.com/";
     }
 
-    public static String getLoginPageURL(){
+    public static String getLoginPageURL() {
         return "http://automationpractice.com/index.php?controller=authentication&back=my-account";
     }
 
-    public static String getLoggedUserPageURL(){
+    public static String getAccountCreationURL() {
+        return "http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation";
+    }
+
+    public static String getLoggedUserPageURL() {
         return "http://automationpractice.com/index.php?controller=my-account";
     }
 
-    public static String getSearchByDressPageURL(){
+    public static String getSearchByDressPageURL() {
         return "http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=dress&submit_search=";
     }
 }
