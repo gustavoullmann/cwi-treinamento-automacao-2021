652ba1347382b43ba988ab66acf931fae2989347
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 18:06:51 -0300

    criados todos os @findbys

--- src/test/java/elementMapper/AccountCreationPageElementMapper.java
+++ src/test/java/elementMapper/AccountCreationPageElementMapper.java
@@ -3,4 +3,32 @@ package elementMapper;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class AccountCreationPageElementMapper {}
+public class AccountCreationPageElementMapper {
+
+    @FindBy(id = "customer_firstname")
+    public WebElement customerFirstname;
+
+    @FindBy(id = "customer_lastname")
+    public WebElement customerLastname;
+
+    @FindBy(id = "passwd")
+    public WebElement password;
+
+    @FindBy(id = "address1")
+    public WebElement address;
+
+    @FindBy(id = "city")
+    public WebElement city;
+
+    @FindBy(id = "id_state")
+    public WebElement idState;
+
+    @FindBy(id = "postcode")
+    public WebElement postcode;
+
+    @FindBy(id = "phone_mobile")
+    public WebElement phoneMobile;
+
+    @FindBy(id = "submitAccount")
+    public WebElement submitAccountButton;
+}
