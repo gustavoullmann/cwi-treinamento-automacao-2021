083e6a25fb78b7a7c3b6cef81080a673c854aa32
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 18:14:45 -0300

    criado construtor, fill search e click submit

--- src/test/java/pageObjects/MyAccountPage.java
+++ src/test/java/pageObjects/MyAccountPage.java
@@ -1,4 +1,23 @@
 package pageObjects;
 
-public class MyAccountPage {
+import elementMapper.MyAccountElementMapper;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.PageFactory;
+import utils.Browser;
+
+public class MyAccountPage extends MyAccountElementMapper {
+
+    public MyAccountPage() {
+        PageFactory.initElements(Browser.getCurrentDriver(), this);
+    }
+
+    @Step("Preencheu a busca")
+    public void fillSearch(String keys) {
+        searchQuery.sendKeys(keys);
+    }
+
+    @Step("Clicou em submeter pesquisa")
+    public void clickSubmitSearchButton() {
+        submitSearch.click();
+    }
 }
