5a0d77be01225c33de947edae1584e50bbf8eaf2
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 00:44:14 -0300

    criado construtor de AccountCreationPage

--- src/test/java/pageObjects/AccountCreationPage.java
+++ src/test/java/pageObjects/AccountCreationPage.java
@@ -1,2 +1,12 @@
-package pageObjects;public class AccountCreationPage {
+package pageObjects;
+
+import elementMapper.AccountCreationPageElementMapper;
+import org.openqa.selenium.support.PageFactory;
+import utils.Browser;
+
+public class AccountCreationPage extends AccountCreationPageElementMapper {
+
+    public AccountCreationPage() {
+        PageFactory.initElements(Browser.getCurrentDriver(),this);
+    }
 }
