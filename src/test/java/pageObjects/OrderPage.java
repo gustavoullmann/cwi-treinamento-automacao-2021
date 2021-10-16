e5a890abf8258ff8d3d8b46dc91e9b4a815aa5de
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 18:16:00 -0300

    criados clicks nos botoes de proceed da diferentes paginas do fluxo

--- src/test/java/pageObjects/OrderPage.java
+++ src/test/java/pageObjects/OrderPage.java
@@ -1,2 +1,43 @@
-package pageObjects;public class OrderPage {
+package pageObjects;
+
+import elementMapper.OrderPageElementMapper;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.PageFactory;
+import utils.Browser;
+
+public class OrderPage extends OrderPageElementMapper {
+
+    public OrderPage() {
+        PageFactory.initElements(Browser.getCurrentDriver(), this);
+    }
+
+    @Step("Clicou em Proceed to checkout")
+    public void clickProceedToCheckoutButton() {
+        proceedToCheckoutButton.click();
+    }
+
+    @Step("Clicou em Proceed to checkout do Step 1")
+    public void clickProceedToCheckoutButtonStep1() {
+        proceedToCheckoutButtonStep1.click();
+    }
+
+    @Step("Clicou em concordo com os termos de serviço")
+    public void clickAgreeToTheTermsServiceCheckBox() {
+        agreeToTheTermsCheckBox.click();
+    }
+
+    @Step("Clicou em Proceed to checkout do Step 2")
+    public void clickProceedToCheckoutButtonStep2() {
+        proceedToCheckoutButtonStep2.click();
+    }
+
+    @Step("Escolheu opção Pay by Bank Wirw")
+    public void clickpayByBankWirePaymentOption() {
+        payByBankWirePaymentOption.click();
+    }
+
+    @Step("Clicou em I confirm my order")
+    public void clickIConfirmMyOrderButton() {
+        confirmOrder.click();
+    }
 }
