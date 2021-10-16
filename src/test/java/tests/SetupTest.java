a533ba99fa26faf10394b5752b2d9b6b8f0e7617
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-16 00:48:42 -0300

    criados novos testes e comentados os antigos

--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -2,7 +2,6 @@ package tests;
 
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,43 +10,67 @@ import pageObjects.*;
 import utils.Browser;
 import utils.Utils;
 
+import java.sql.SQLOutput;
+
 import static org.junit.Assert.*;
 
-@Feature("Testes site de ecommerce")
+@Feature("Testes site de e-commerce http://automationpractice.com/")
 public class SetupTest extends BaseTests{
 
     @Test
-    @Story("Abrir o site")
-    public void testOpeningBrowserAndLoadingPage(){
-        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseURL()));
-        System.out.println("Abrimos o navegador e carregamos a url!");
+    @Story("Carregar a homepage do site")
+    public void testLoadHomePage() {
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getHomePageURL()));
+        System.out.println("Abrimos o navegador e carregamos a url da homepage!");
     }
 
     @Test
-    @Story("Realizar o login")
-    public void testLogin(){
-        HomePage home = new HomePage();
-        LoginPage login = new LoginPage();
+    @Story("Carregar a página de criação de cadastro")
+    public void testLoadAccountCreation() {
+        HomePage homePage = new HomePage();
+        LoginPage loginPage = new LoginPage();
 
-        home.clickBtnLogin();
-        System.out.println("Clicou em Sign In e direcionou para a página de login");
+        homePage.clickBtnLogin();
         assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getLoginPageURL()));
+        System.out.println("Abrimos o navegador e carregamos a url da página de login!");
 
-        login.fillEmail();
-        System.out.println("Inseriu e-mail");
-        login.fillPasswd();
-        System.out.println("Inseriu senha");
-        login.clickBtnSubmitLogin();
+        loginPage.fillAccountCreationEmail();
+        System.out.println("Preencheu o campo de e-mail");
+        loginPage.createAnAccountClickButton();
+        System.out.println("Clicou em Create an account");
 
-        System.out.println("Click sign in");
-        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getLoggedUserPageURL()));
-        System.out.println("Logado com sucesso");
-        assertTrue(Browser.getCurrentDriver().findElement(By.className("page-heading")).getText().contains("MY ACCOUNT"));
-        System.out.println("Pagina de usuário carregada com sucesso");
+        WebDriverWait wait = new WebDriverWait(Browser.getCurrentDriver(), 10);
+        wait.until(ExpectedConditions.urlContains("#account-creation"));
+
+        assertEquals(Browser.getCurrentDriver().getCurrentUrl(), Utils.getAccountCreationURL());
+        System.out.println("Confirmou a URL");
     }
 
 //    @Test
-//    public void testSearchProduct(){
+//    @Story("Realizar o login")
+//    public void testLogin() {
+//        HomePage home = new HomePage();
+//        LoginPage login = new LoginPage();
+//
+//        home.clickBtnLogin();
+//        System.out.println("Clicou em Sign In e direcionou para a página de login");
+//        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getLoginPageURL()));
+//
+//        login.fillEmail();
+//        System.out.println("Inseriu e-mail");
+//        login.fillPasswd();
+//        System.out.println("Inseriu senha");
+//        login.clickBtnSubmitLogin();
+//
+//        System.out.println("Click sign in");
+//        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getLoggedUserPageURL()));
+//        System.out.println("Logado com sucesso");
+//        assertTrue(Browser.getCurrentDriver().findElement(By.className("page-heading")).getText().contains("MY ACCOUNT"));
+//        System.out.println("Pagina de usuário carregada com sucesso");
+//    }
+//
+//    @Test
+//    public void testSearchProduct() {
 //        HomePage home = new HomePage();
 //        SearchPageResult search = new SearchPageResult();
 //
@@ -64,44 +87,42 @@ public class SetupTest extends BaseTests{
 //
 //        System.out.println("executou a busca");
 //    }
-
-    @Test
-    @Story("Acessar Categoria")
-    public void testAccessCategoryTShirts(){
-        HomePage home = new HomePage();
-        CategoryPage category = new CategoryPage();
-
-        home.clickCategoryTShirts();
-        assertTrue(category.isPageTShirts());
-    }
-
-    @Test
-    @Story("Adicionar página de Produto")
-    public void testAddProductToProductPage(){
-        testAccessCategoryTShirts();
-
-        CategoryPage category = new CategoryPage();
-        ProductPage pdp = new ProductPage();
-        String nameProductCategory = category.getProductNameCategory();
-        category.clickProductAddToProductPage();
-        assertEquals(pdp.getProductNamePDP(), nameProductCategory);
-    }
-
-    @Test
-    @Story("Adicionar Produto ao Carrinho")
-    public void testAddProductToCartPage() {
-        testAddProductToProductPage();
-
-        ProductPage pdp = new ProductPage();
-        CartPage cart = new CartPage();
-
-        String nameProductPDP = pdp.getProductNamePDP();
-        pdp.ClickButtonAddToCart();
-        WebDriverWait wait = new WebDriverWait(Browser.getCurrentDriver(), 10);
-        wait.until(ExpectedConditions.elementToBeClickable(pdp.clearFix));
-        pdp.clickButtonModalProceedToCheckout();
-        assertEquals(cart.getNameProductCart(), nameProductPDP);
-    }
+//
+//    @Test
+//    @Story("Acessar Categoria")
+//    public void testAccessCategoryTShirts() {
+//        HomePage home = new HomePage();
+//        CategoryPage category = new CategoryPage();
+//
+//        home.clickCategoryTShirts();
+//        assertTrue(category.isPageTShirts());
+//    }
+//
+//    @Test
+//    @Story("Adicionar página de Produto")
+//    public void testAddProductToProductPage() {
+//        testAccessCategoryTShirts();
+//
+//        CategoryPage category = new CategoryPage();
+//        ProductPage pdp = new ProductPage();
+//        String nameProductCategory = category.getProductNameCategory();
+//        category.clickProductAddToProductPage();
+//        assertEquals(pdp.getProductNamePDP(), nameProductCategory);
+//    }
+//
+//    @Test
+//    @Story("Adicionar Produto ao Carrinho")
+//    public void testAddProductToCartPage() {
+//        testAddProductToProductPage();
+//
+//        ProductPage pdp = new ProductPage();
+//        CartPage cart = new CartPage();
+//
+//        String nameProductPDP = pdp.getProductNamePDP();
+//        pdp.ClickButtonAddToCart();
+//        WebDriverWait wait = new WebDriverWait(Browser.getCurrentDriver(), 10);
+//        wait.until(ExpectedConditions.elementToBeClickable(pdp.clearFix));
+//        pdp.clickButtonModalProceedToCheckout();
+//        assertEquals(cart.getNameProductCart(), nameProductPDP);
+//    }
 }
-
-//teste new branch
