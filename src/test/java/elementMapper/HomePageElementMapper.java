814142e9beef067efa37f4d5d3a1b531946172a7
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-17 18:37:23 -0300

    exclusão de mappers não utilizados

--- src/test/java/elementMapper/HomePageElementMapper.java
+++ src/test/java/elementMapper/HomePageElementMapper.java
@@ -7,13 +7,4 @@ public class HomePageElementMapper {
 
     @FindBy(className = "login")
     public WebElement login;
-
-    @FindBy(id = "search_query_top")
-    public WebElement searchQueryTop;
-
-    @FindBy(name = "submit_search")
-    public WebElement submitSearch;
-
-    @FindBy(css = "#block_top_menu .sf-menu li:nth-child(3) a[title=T-shirts]")
-    public WebElement menuTShirts;
 }
